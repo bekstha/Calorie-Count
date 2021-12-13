b92de7b05271e4af1532bc5ce82d1b3752f310ea
Bibek Shrestha <bibek.shrestha@metropolia.fi>
2021-12-13 17:15:31 +0200

    typo

--- app/src/main/java/fi/metropolia/practisecalorie/History.java
+++ app/src/main/java/fi/metropolia/practisecalorie/History.java
@@ -5,7 +5,6 @@ import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
